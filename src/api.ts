e63282be112d818a7d34038c13e41326409640e5
Rishal <Rishal.mhd009@gmail.com>
2023-08-27 03:00:43 +0530

    getting the data

--- src/api.ts
+++ src/api.ts
@@ -5,8 +5,8 @@ import pool from "./db";
 export const t = initTRPC.create();
 
 export const appRouter = t.router({
-  getAllBooks: t.procedure.input(z.string()).query(() => {
-    const books = pool.query("SELECT * FROM books");
+  getAllBooks: t.procedure.query(async () => {
+    const books = await pool.query("SELECT * FROM books");
     return books;
   }),
 });
